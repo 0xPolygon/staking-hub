c87c030a775081a61b287c5951d46b9616e4f16d
Zero Ekkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2023-12-19 11:24:42 +0100

    feat(strategy): track subscriptions

--- src/Strategy.sol
+++ src/Strategy.sol
@@ -5,21 +5,21 @@ abstract contract Strategy {
     /// @dev Attempts deleting the first Until to save on gas and clean state.
     /// @dev Use with _createUntil.
     /// @dev Saves on gas as long as subscriptions expire faster than new ones are created, and Services do not Unsubscribe Users before Until.
-    /// @dev Saves ~350 gas. Trying costs ~145 gas.
-    modifier gasSaving() {
-        if (head != 0 && block.timestamp >= head) {
-            uint256 headId = head;
-            uint256 nextUntil = list[head].next;
+    /// @dev Saves ~300 G/Tx. Trying costs ~170 G.
+    modifier gasSaving(Subscriptions storage $) {
+        if ($.head != 0 && block.timestamp >= $.head) {
+            uint256 headId = $.head;
+            uint256 nextUntil = $.list[$.head].next;
 
             if (nextUntil != 0) {
-                list[nextUntil].prev = 0;
+                $.list[nextUntil].prev = 0;
             } else {
-                tail = 0;
+                $.tail = 0;
             }
 
-            head = nextUntil;
+            $.head = nextUntil;
 
-            delete list[headId];
+            delete $.list[headId];
         }
         _;
     }
@@ -37,79 +37,81 @@ abstract contract Strategy {
         uint256 prev;
     }
 
-    //struct Subscriptions {
-    mapping(uint256 timestampAsId => Until) list; // Doubly linked list
-    uint256 head;
-    uint256 tail;
-    // For subscription extension functionality
-    mapping(uint256 service => uint256 untilId) _lastUntil;
-    //}
+    struct Subscriptions {
+        mapping(uint256 timestampAsId => Until) list; // Doubly linked list
+        uint256 head;
+        uint256 tail;
+        // For subscription extension functionality
+        mapping(uint256 service => uint256 untilId) _lastUntil;
+    }
 
-    //mapping(address staker => Subscriptions) subscriptions;
+    mapping(address staker => Subscriptions) subscriptions;
 
     // Function to create a new Until
-    function _createUntil(uint256 _timestamp) private gasSaving {
-        uint256 current = tail;
+    function _createUntil(Subscriptions storage $, uint256 _timestamp) private gasSaving($) {
+        uint256 current = $.tail;
         uint256 newId = _timestamp; // Unique ID for the new Until
 
         // Check if the list is empty or the new Until is the latest
-        if (current == 0 || list[current].timestamp < _timestamp) {
-            Until storage newUntil = list[newId];
+        if (current == 0 || $.list[current].timestamp < _timestamp) {
+            Until storage newUntil = $.list[newId];
             newUntil.timestamp = _timestamp;
             newUntil.prev = current; // If list is empty, newUntil.prev is 0; otherwise, it's the old tail
             newUntil.next = 0; // The new node is now the tail, so its next is 0
 
             if (current != 0) {
-                list[current].next = newId; // Update the old tail's next to point to the new tail
+                $.list[current].next = newId; // Update the old tail's next to point to the new tail
             }
 
-            tail = newId; // Update the tail to the new node
+            $.tail = newId; // Update the tail to the new node
 
-            if (head == 0) {
-                head = newId; // If the list was empty, the new node is also the head
+            if ($.head == 0) {
+                $.head = newId; // If the list was empty, the new node is also the head
             }
 
             return;
         }
 
         // Traverse backwards to find the correct position to insert the new Until
-        while (list[current].prev != 0 && list[list[current].prev].timestamp > _timestamp) {
-            current = list[current].prev;
+        while ($.list[current].prev != 0 && $.list[$.list[current].prev].timestamp > _timestamp) {
+            current = $.list[current].prev;
         }
 
-        Until storage newUntil = list[newId];
+        Until storage newUntil = $.list[newId];
         newUntil.timestamp = _timestamp;
-        newUntil.prev = list[current].prev;
+        newUntil.prev = $.list[current].prev;
         newUntil.next = current;
 
         if (newUntil.prev != 0) {
-            list[newUntil.prev].next = newId;
+            $.list[newUntil.prev].next = newId;
         } else {
-            head = newId; // Update the head if the new node is now the first node
+            $.head = newId; // Update the head if the new node is now the first node
         }
 
-        list[current].prev = newId;
+        $.list[current].prev = newId;
     }
 
     // Function to add a service; can create a new Until, remove a Service, remove an Until
-    function addService(uint256 _timestamp, uint256 _service) internal {
+    function addService(address _staker, uint256 _timestamp, uint256 _service) internal {
+        Subscriptions storage $ = subscriptions[_staker];
+
         require(_timestamp != 0, "Invalid timestamp");
         require(_timestamp > block.timestamp, "Invalid timestamp");
 
         uint256 _untilId = _timestamp; // Unique ID for the new Until
 
         // If subscription exists, require that the new timestamp is greater than the current one
-        if (_lastUntil[_service] > block.timestamp) {
-            require(_timestamp > list[_lastUntil[_service]].timestamp, "Timestamp must be greater than the current one");
+        if ($._lastUntil[_service] > block.timestamp) {
+            require(_timestamp > $.list[$._lastUntil[_service]].timestamp, "Timestamp must be greater than the current one");
             // Remove service from the old Until
-            removeService(_lastUntil[_service], _service);
+            removeService(_staker, $._lastUntil[_service], _service);
         }
 
-        Until storage until = list[_untilId];
+        Until storage until = $.list[_untilId];
 
         // If Until does not exist, create it
         if (until.timestamp == 0) {
-            _createUntil(_timestamp);
+            _createUntil($, _timestamp);
         }
 
         if (!until.services[_service]) {
@@ -118,13 +120,15 @@ abstract contract Strategy {
         }
 
         // Update service tracker
-        _lastUntil[_service] = _untilId;
+        $._lastUntil[_service] = _untilId;
     }
 
     // Function to remove a service for an existing Until; can remove an Until
-    function removeService(uint256 _untilId, uint256 _service) internal {
+    function removeService(address _staker, uint256 _untilId, uint256 _service) internal {
+        Subscriptions storage $ = subscriptions[_staker];
+
         require(_untilId != 0, "Invalid Until");
-        Until storage until = list[_untilId];
+        Until storage until = $.list[_untilId];
         require(until.timestamp != 0, "Until does not exist");
         require(until.services[_service], "Service does not exist");
 
@@ -133,15 +137,15 @@ abstract contract Strategy {
 
         // If Until has no more services, remove it
         if (until.servicesCounter == 0) {
-            _deleteUntil(_untilId);
+            _deleteUntil($, _untilId);
         }
     }
 
     // Function to remove an Until; requires that all services have been removed or the Until has expired
-    function _deleteUntil(uint256 _untilId) private {
+    function _deleteUntil(Subscriptions storage $, uint256 _untilId) private {
         require(_untilId != 0, "Cannot remove sentinel Until");
 
-        Until storage untilToRemove = list[_untilId];
+        Until storage untilToRemove = $.list[_untilId];
 
         // Check if Until exists in the list
         require(untilToRemove.timestamp != 0, "Until does not exist");
@@ -154,19 +158,19 @@ abstract contract Strategy {
 
         // Adjust pointers of adjacent Untils
         if (prevUntil != 0) {
-            list[prevUntil].next = nextUntil;
+            $.list[prevUntil].next = nextUntil;
         } else {
-            head = nextUntil; // Update head if removing the first Until
+            $.head = nextUntil; // Update head if removing the first Until
         }
 
         if (nextUntil != 0) {
-            list[nextUntil].prev = prevUntil;
+            $.list[nextUntil].prev = prevUntil;
         } else {
-            tail = prevUntil; // Update tail if removing the last Until
+            $.tail = prevUntil; // Update tail if removing the last Until
         }
 
         // Delete the Until from the mapping
-        delete list[_untilId];
+        delete $.list[_untilId];
     }
 
     // ====== End of AI generated code ====== //
@@ -189,14 +193,16 @@ abstract contract Strategy {
 
     /// @dev Called buy the Hub when a Staker subscribes.
     function onSubscribe(address staker, uint256 service, uint256 until) external {
-        addService(until, service);
+        addService(staker, until, service);
     }
 
     /// @notice Withdraws funds from the Strategy.
     /// @notice Funds are locked until no active subscription remain (expired or unsubbed from).
     /// @dev Called by a Staker.
     function withdraw() external {
-        require(tail != 0 && block.timestamp >= tail || head == 0 && tail == 0, "Funds locked");
+        Subscriptions storage $ = subscriptions[msg.sender];
+
+        require($.tail != 0 && block.timestamp >= $.tail || $.head == 0 && $.tail == 0, "Funds locked");
 
         uint256 value = _balances[msg.sender];
         _balances[msg.sender] = 0;
@@ -208,7 +214,9 @@ abstract contract Strategy {
     /// @notice Notifies the Strategy that the Staker has unsubscribed from a Service.
     /// @dev Called by the Hub.
     function onUnsubscribe(address staker, uint256 service) external {
-        removeService(_lastUntil[service], service);
+        Subscriptions storage $ = subscriptions[staker];
+
+        removeService(staker, $._lastUntil[service], service);
     }
 
     /// @dev Called by the Hub.
