2b5ce0fc1bd38178e8608a56fa9e6e3c739a5fcf
Zero Ekkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2023-12-20 17:30:23 +0100

    docs(slasher): document entire code

--- src/interface/ISlasher.sol
+++ src/interface/ISlasher.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-/// @notice Purpose: Service contract can be upgradeable while not introducting risk with slashing.
+/// @title Slasher
+/// @author Polygon Labs
+/// @notice A Slasher separates the freezing and slashing functionality from a Service.
 interface ISlasher {
-    /// @notice Freezes a Staker.
+    /// @notice Temporarily prevents a Staker from taking action.
+    /// @notice This period can be used to prove the Staker should be slashed.
+    /// @dev Called by TODO
     /// @dev Calls onFreeze on the Hub.
     function freeze(address staker) external;
 
-    /// @notice Slashes a Staker.
+    /// @notice Takes a portion of a Staker's funds away.
+    /// @notice The Staker must be frozen first.
+    /// @dev Called by TODO
     /// @dev Calls onSlash on the Hub.
     function slash(address staker, uint8 percentage) external;
 }
