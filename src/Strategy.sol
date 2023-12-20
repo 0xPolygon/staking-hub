3a35c59d7faa8a47ed736984f6e5f6b826b8ae8d
Zero Ekkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2023-12-20 18:28:35 +0100

    docs(strategy): document entire code

--- src/Strategy.sol
+++ src/Strategy.sol
@@ -3,42 +3,33 @@ pragma solidity 0.8.23;
 
 import {SubscriptionTracker} from "src/lib/SubscriptionTracker.sol";
 
-// Note:
-// This is internal subscription tracking.
-//
-// An alternative is to record each service the user subscribes to
-// and then check if all the subscriptions have expired
-// via the Hub (subscribedToService) on withdraw.
-
+/// @title Strategy
+/// @author Polygon Labs
+/// @notice A Strategy holds and manages Stakers' funds.
+/// @notice A Staker deposits funds into the Strategy before subscribing to a Services that uses the Strategy.
 abstract contract Strategy is SubscriptionTracker {
-    address private constant HUB = address(0);
-
-    // TODO Can be POL, ERC20s, NFTs.
+    // TODO Allow POL, ERC20s, NFTs.
     mapping(address staker => uint256 balance) private _balances;
 
-    /// @notice Adds funds to be available for restaking.
-    /// @dev Called by a Staker.
+    /// @return The amount of funds the Staker has in the Strategy.
+    function balanceOf(address staker) public view returns (uint256) {
+        return _balances[staker];
+    }
+
+    /// @notice Adds funds to be available to a Staker for restaking.
+    /// @dev Called by the Staker.
     function deposit() external payable {
         _balances[msg.sender] += msg.value;
     }
 
-    /// @return The amount of funds the Staker has in the Strategy.
-    function balanceOf(address staker) external view returns (uint256) {
-        return _balances[staker];
-    }
-
-    /// @dev Called buy the Hub when a Staker subscribes.
-    function onSubscribe(address staker, uint256 service, uint256 until) external {
-        addSubscription(staker, until, service);
-    }
-
-    /// @notice Withdraws funds from the Strategy.
-    /// @notice Funds are locked until no active subscription remain (expired or unsubbed from).
-    /// @dev Called by a Staker.
+    /// @notice Retrieves all Staker's funds from the Strategy.
+    /// @notice The Staker must be unsubscribed from all Services first.
+    /// @notice A Staker is unsubscribed from a Service if the Staker unsubscribed via the Hub, or the subscription has expired.
+    /// @dev Called by the Staker.
     function withdraw() external {
         Subscriptions storage $ = subscriptions[msg.sender];
 
-        require($.tail != 0 && block.timestamp >= $.tail || $.head == 0 && $.tail == 0, "Funds locked");
+        require($.tail != 0 && block.timestamp >= $.tail || $.head == 0 && $.tail == 0, "Cannot withdraw because there are active subscriptions");
 
         uint256 value = _balances[msg.sender];
         _balances[msg.sender] = 0;
@@ -47,21 +38,36 @@ abstract contract Strategy is SubscriptionTracker {
         require(success, "Withdrawal failed");
     }
 
-    /// @notice Notifies the Strategy that the Staker has unsubscribed from a Service.
-    /// @dev Called by the Hub.
+    /// @notice Stats tracking a subscription.
+    /// @dev Called by the Hub when a Staker has subscribed to a Services that uses the Strategy.
+    function onSubscribe(address staker, uint256 service, uint256 until) external {
+        addSubscription(staker, until, service);
+    }
+
+    /// @notice Stops tracking a subscription.
+    /// @dev Called by the Hub when a Staker has unsubscribed from a Services that uses the Strategy.
     function onUnsubscribe(address staker, uint256 service) external {
         Subscriptions storage $ = subscriptions[staker];
         removeSubscription(staker, $._lastUntil[service], service);
     }
 
-    //function onFreeze(address staker) external virtual {}
-
+    /// @notice Takes a portion of a Staker's funds away.
+    /// @dev Called by the Hub when a Staker has subscribed to a Services that uses the Strategy.
     function onSlash(address staker, uint8 percentage) external virtual {
         uint256 amount = _balances[staker] * percentage;
+
         _balances[staker] -= amount;
 
-        // Burn, for now.
+        // TODO What to do with the slashed funds?
+        // Burn the slashed amount.
         (bool success,) = address(0).call{value: amount}("");
         require(success, "Slash failed");
     }
+
+    // Note:
+    // The current implementation uses internal subscription tracking.
+    //
+    // An alternative approach is to record each Service the Staker subscribes to,
+    // and check if all subscriptions have expired via the Hub on withdraw.
+    // Similar approach can be imolemented internally as well.
 }
