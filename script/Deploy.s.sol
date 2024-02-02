dd2e6b8b541f217b361799c996b3290f98917f9a
gretzke <daniel@gretzke.de>
2024-02-02 01:11:38 +0100

    remove default contract

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,14 +2,9 @@
 pragma solidity 0.8.23;
 
 import "forge-std/Script.sol";
-import "script/deployers/CounterDeployer.s.sol";
 
-contract Deploy is Script, CounterDeployer {
+contract Deploy is Script {
     using stdJson for string;
 
-    function run() public {
-        address proxyAdmin = address(1);
-        uint256 initialNumber = 5;
-        deployCounterTransparent(proxyAdmin, initialNumber);
-    }
+    function run() public {}
 }
