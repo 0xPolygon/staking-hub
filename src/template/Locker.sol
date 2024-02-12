7342b7319b92c75ba0aabc9adf1dac29a36927f0
Zero Ekkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2024-02-12 15:00:09 +0100

    fix(locker): accounting

--- src/template/Locker.sol
+++ src/template/Locker.sol
@@ -46,11 +46,11 @@ abstract contract Locker is ILocker {
     }
 
     function deposit(uint256 amount) external burner {
-        (uint256 balanceIncrease, uint256 votingPowerIncrease) = _deposit(amount);
-        _staker[msg.sender].balance += balanceIncrease;
-        _totalSupply += balanceIncrease;
-        _staker[msg.sender].votingPower += votingPowerIncrease;
-        _totalVotingPower += votingPowerIncrease;
+        (uint256 newBalance, uint256 newTotalSupply, uint256 newVotingPower, uint256 newTotalVotingPower) = _deposit(amount);
+        _staker[msg.sender].balance = newBalance;
+        _totalSupply = newTotalSupply;
+        _staker[msg.sender].votingPower = newVotingPower;
+        _totalVotingPower = newTotalVotingPower;
     }
 
     function onSubscribe(address staker, uint256 service, uint8 maxSlashPercentage, uint8 recommendedRisk) external burner {
@@ -73,7 +73,7 @@ abstract contract Locker is ILocker {
     function initiateWithdrawal(uint256 amount, bool force) public burner {
         if (!force) require(_staker[msg.sender].initialWithdrawalAmount == 0, "Withrawal already initiated");
         require(amount != 0, "Invalid amount");
-        require(_compareAmounts(amount, _safeBalanceOf(msg.sender)) != Relation.GT, "Amount exceeds safe balance");
+        require(_compareAmount(amount, _safeBalanceOf(msg.sender)) != Relation.GT, "Amount exceeds safe balance");
         _staker[msg.sender].withdrawableFrom = block.timestamp + STAKER_WITHDRAWAL_DELAY;
         _staker[msg.sender].initialWithdrawalAmount = amount;
     }
@@ -82,13 +82,13 @@ abstract contract Locker is ILocker {
         amount = _staker[msg.sender].initialWithdrawalAmount;
         require(amount != 0, "Withrawal not initiated");
         require(_staker[msg.sender].withdrawableFrom > block.timestamp, "Cannot withdraw at this time");
-        if (_compareAmounts(amount, _safeBalanceOf(msg.sender)) == Relation.GT) amount = _safeBalanceOf(msg.sender);
+        if (_compareAmount(amount, _safeBalanceOf(msg.sender)) == Relation.GT) amount = _safeBalanceOf(msg.sender);
         require(amount != 0, "Nothing to withdraw");
-        (uint256 balanceDecrease, uint256 votingPowerDecrease) = _withdraw(amount);
-        _staker[msg.sender].balance -= balanceDecrease;
-        _totalSupply -= balanceDecrease;
-        _staker[msg.sender].votingPower -= votingPowerDecrease;
-        _totalVotingPower -= votingPowerDecrease;
+        (uint256 newBalance, uint256 newTotalSupply, uint256 newVotingPower, uint256 newTotalVotingPower) = _withdraw(amount);
+        _staker[msg.sender].balance = newBalance;
+        _totalSupply = newTotalSupply;
+        _staker[msg.sender].votingPower = newVotingPower;
+        _totalVotingPower = newTotalVotingPower;
     }
 
     function balanceOf(address staker) public view returns (uint256 balance) {
@@ -112,11 +112,17 @@ abstract contract Locker is ILocker {
     }
 
     function _burn(uint256 percenage) internal virtual;
-    function _deposit(uint256 amount) internal virtual returns (uint256 balanceIncrease, uint256 votingPowerIncrease);
+    function _deposit(uint256 amount)
+        internal
+        virtual
+        returns (uint256 newBalance, uint256 newTotalSupply, uint256 newVotingPower, uint256 newTotalVotingPower);
     function _onSubscribe(address staker, uint256 service, uint256 maxSlashPercentage, uint8 recommendedRisk) internal virtual;
     function _onUnsubscribe(address staker, uint256 service, uint8 maxSlashPercentage) internal virtual;
-    function _compareAmounts(uint256 a, uint256 b) internal virtual returns (Relation);
-    function _withdraw(uint256 amount) internal virtual returns (uint256 balanceDecrease, uint256 votingPowerDecrease);
+    function _compareAmount(uint256 a, uint256 b) internal virtual returns (Relation);
+    function _withdraw(uint256 amount)
+        internal
+        virtual
+        returns (uint256 newBalance, uint256 newTotalSupply, uint256 newVotingPower, uint256 newTotalVotingPower);
     function _balanceOf(address staker, uint256 currentBalance, uint256 slashedPercentage) internal view virtual returns (uint256 balance);
     function _safeBalanceOf(address staker) internal view virtual returns (uint256 safeBalance);
 }
